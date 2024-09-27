ede89e0e4e8202abbd635055a5ba60c45db210f6
fittechgym <fittechgymmanagement@gmail.com>
2024-09-27 17:25:38 +0530

    Create routes using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,53 @@
-import {HomeLayout,Landing,Error,Products,SingleProduct,Cart,About,Register,Login,Checkout,Orders} from './pages'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import {HomeLayout,Landing,Error,Products,SingleProduct,Cart,About,Register,Login,Checkout,Orders} from './pages';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout/>,
+      errorElement: <Error/>,
+      children: [
+        {
+          index: true,
+          element: <Landing/>,
+        },
+        {
+          path: 'products',
+          element: <Products/>,
+        },
+        {
+          path: 'products/:id',
+          element:<SingleProduct/>,
+        },
+        {
+          path: 'cart',
+          element: <Cart/>,
+        },
+        {
+          path: 'about',
+          element: <About/>,
+        },
+        {
+          path: 'checkout',
+          element: <Checkout/>,
+        },{
+          path: 'orders',
+          element: <Orders/>,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login/>,
+      errorElement: <Error/>,
+    },
+    {
+      path: '/register',
+      element: <Register/>,
+      errorElement: <Error/>,
+    },
+]);
 
 const App = () => {
   return (
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,13 @@
+import {Outlet} from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h1 className="text-4xl">HomeLayout</h1>;
+  return (
+    <>
+    <nav>
+      <span className="txt-4xl text-primary">CStore</span>
+    </nav>
+    <Outlet/>
+    </>
+  )
 };
 export default HomeLayout;
