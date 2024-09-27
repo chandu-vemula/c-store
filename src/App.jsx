5bfc600c3121bee7f28ad90322d8bad21fed46e6
fittechgym <fittechgymmanagement@gmail.com>
2024-09-27 10:40:37 +0530

    Named import all the pages to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {HomeLayout,Landing,Error,Products,SingleProduct,Cart,About,Register,Login,Checkout,Orders} from './pages'
+
 const App = () => {
   return (
     <h1 className="text-7xl font-bold underline">
